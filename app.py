76110ab4e45faefbf469a1025c39fc86912c68e0
Harvin Zúniga <harvin.zuniga@gmail.com>
2020-10-30 05:40:00 +0000

    connected to MongoDB and rendering template

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
-from flask import Flask
-# , render_template, redirect, request, url_for
-# from flask_pymongo import PyMongo
-# from bson.objectid import ObjectId
+from flask import (
+    Flask, flash, render_template, 
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 
 if os.path.exists("env.py"):
     import env 
@@ -10,10 +12,24 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
+# Adding enviroment properties to the project, all this is seted up in Heroku
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+# Adding Mongo Construtor
+mongo = PyMongo(app)
 
 @app.route("/")
-def hello():
-    return "Hello World...again!"
+@app.route("/get_tasks")
+
+def get_tasks():
+    tasks = mongo.db.tasks.find()
+    # with the rendering of the template, we'll pass that tasks 
+    # variable through to the template: tasks=tasks. The first 'tasks'
+    # is what the template will use, and that's equal to the second 'tasks',
+    # which is our variable defined above.
+    return render_template("tasks.html", tasks=tasks)
 
 
 if __name__ == "__main__":
