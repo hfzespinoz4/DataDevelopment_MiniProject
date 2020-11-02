87e5558e13e59647dcb3c03496ead02769cca3df
Harvin Zúniga <harvin.zuniga@gmail.com>
2020-11-02 21:21:30 +0000

    Adding Register Template

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 if os.path.exists("env.py"):
@@ -20,16 +21,21 @@ app.secret_key = os.environ.get("SECRET_KEY")
 # Adding Mongo Construtor
 mongo = PyMongo(app)
 
+
 @app.route("/")
 @app.route("/get_tasks")
-
 def get_tasks():
     tasks = mongo.db.tasks.find()
-    # with the rendering of the template, we'll pass that tasks 
+    # rendering of the template, we'll pass that tasks
     # variable through to the template: tasks=tasks. The first 'tasks'
     # is what the template will use, and that's equal to the second 'tasks',
     # which is our variable defined above.
     return render_template("tasks.html", tasks=tasks)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
